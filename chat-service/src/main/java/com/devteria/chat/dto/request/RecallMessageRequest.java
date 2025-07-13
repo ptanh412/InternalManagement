c8cf5d5bb742a610de374b2a9ca2a7afec778321
Devteria <devteriadotcom@gmail.com>
2025-07-13 17:06:36 +0700

    socket sessions management

---
+++ chat-service/src/main/java/com/devteria/chat/dto/request/RecallMessageRequest.java
@@ -0,0 +1,16 @@
+package com.devteria.chat.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RecallMessageRequest {
+    String messageId;
+    String conversationId;
+    String recallType; // "self" or "everyone"
+}
